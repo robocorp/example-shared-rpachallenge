517de32ab8f683622703a7b2e64e391d6acefef1
Mika Hänninen <mika.hanninen@gmail.com>
2022-04-21 16:12:33 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -11,19 +11,15 @@ cd /D %scriptPath%
 set /P version="Select package version ['X.Y.Z' OR empty to quit]: "
 IF "%version%"=="" goto :END
 
-rem for /f %%a in ('git config -f .gitmodules submodule.modules.branch') do set "previous_version=%%a"
-rem IF "%previous_version%"=="" (goto :INIT_MODULES) else (goto :CONTINUE)
-
 if not exist "%SHARED_DIRECTORY%\" (
-    echo NOT EXISTS - Updating to Shared version %version% ...
+    echo Retrieving Shared repository version %version% ...
     git subtree add --prefix shared %SHARED_REPOSITORY% %version% --squash || goto :ERROR_BRANCH
     goto :PRINT_VERSION
 ) else (
-    echo EXISTS - Updating to Emerson Common package version %version% ...
-    git subtree pull --prefix shared https://github.com/robocorp/example-shared-code-common %version% --squash || goto :ERROR_BRANCH
+    echo Updating to Shared repository version %version% ...
+    git subtree pull --prefix shared %SHARED_REPOSITORY% %version% --squash || goto :ERROR_BRANCH
 )
 goto :PRINT_VERSION
-
 
 
 :PRINT_VERSION
@@ -33,7 +29,6 @@ goto :END
 
 :ERROR_BRANCH
 echo Error! Git branch '%version%' does not exist or no access to repository
-IF "%previous_version%"=="" (goto end) else (git subtree set-branch --branch %previous_version% modules)
 goto :END
 
 :END
