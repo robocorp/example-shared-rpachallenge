1538411a6fa2c9331404b66871980ba5f20b679d
Mika Hänninen <mika.hanninen@gmail.com>
2022-09-23 09:53:20 +0300

    Squashed 'shared/' changes from f9ad49d..d34a794
    d34a794 update release.bat
    
    git-subtree-dir: shared
    git-subtree-split: d34a79458025af2aaac15500ed521a08a916e403

--- release.bat
+++ release.bat
@@ -5,12 +5,12 @@ SET scriptPath=%~dp0
 SET scriptPath=%scriptPath:~0,-1%
 CD /D %scriptPath%
 
-REM TODO. output somehow the tag currently in use
+REM TODO. output somehow the tag currently in use. git tag ?
 REM TODO. need to verify that version is correct
 SET /P release="Set release version ['X.Y.Z' or empty for no release]: "
 IF "%release%"=="" goto :END
 
-SET releasetext=Release %release% for Shared Common
+SET releasetext=Release %release% for Shared Code Common
 git tag -a %release% -m "%releasetext%"
 git push origin refs/tags/%release%
 
